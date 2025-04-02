f125a3958adeaa7bd36b8b9bb1d1624a05345200
Dan <daniel_vazrobk@hotmail.com>
2025-04-02 12:45:13 -0600

    Se quito la variable :page en la ruta videos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,22 @@ export const routes: Routes = [
     path: 'microservice-youtube',
     loadComponent: () =>
       import('./microservice-youtube/microservice-youtube.component'),
+    children: [
+      {
+        path: 'videos',
+        loadComponent: () =>
+          import('./microservice-youtube/pages/videos/videos.component'),
+      },
+      {
+        path: 'favorites/:page',
+        loadComponent: () =>
+          import('./microservice-youtube/pages/favorites/favorites.component'),
+      },
+    ],
   },
   {
     path: '',
-    redirectTo: '/microservice-youtube',
+    redirectTo: '/microservice-youtube/videos',
     pathMatch: 'full',
   },
 ];
