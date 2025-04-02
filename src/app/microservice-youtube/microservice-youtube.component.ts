a38ff9bcacff05aabf6efba5982c901bb2a13d1a
Dan <daniel_vazrobk@hotmail.com>
2025-04-02 12:59:05 -0600

    Se importo el RouterOutlet en el componente principal

--- src/app/microservice-youtube/microservice-youtube.component.ts
+++ src/app/microservice-youtube/microservice-youtube.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../Shared/Components/header/header.component';
 import { MenuComponent } from '../Shared/Components/menu/menu.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-microservice-youtube',
-  imports: [HeaderComponent, MenuComponent],
+  imports: [HeaderComponent, MenuComponent, RouterOutlet],
   templateUrl: './microservice-youtube.component.html',
   styleUrl: './microservice-youtube.component.css',
 })
