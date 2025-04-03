f6dd4fa6a6164144918e92620010f25fcf6026d3
Dan <daniel_vazrobk@hotmail.com>
2025-04-03 13:45:13 -0600

    Se modifico el app.config para que inicie Keycloak antes de cargar la aplicacion

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,13 +6,24 @@ import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { KeycloakService } from './Core/Services/Keycloak/keycloak.service';
+import { authTokenInterceptor } from './Core/Interceptors/auth-token.interceptor';
+
+const keycloakService = new KeycloakService();
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch(), withInterceptors([authTokenInterceptor])),
+    { provide: KeycloakService, useValue: keycloakService },
   ],
 };
+
+// Inicializar Keycloak antes de arrancar la aplicación
+export function initKeycloak(): Promise<void> {
+  return keycloakService.init();
+}
