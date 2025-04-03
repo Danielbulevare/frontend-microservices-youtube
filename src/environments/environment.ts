93958bcc8ac55a93fa28991e45cdd1a814521284
Dan <daniel_vazrobk@hotmail.com>
2025-04-03 13:47:36 -0600

    Se añadio a las variables de entorno la url del api que guarda los video fvoritos del usuario

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   KEY: 'AIzaSyD4z1FOI9Yn29tPBY5SPnNUdGYSpmnV4_8',
   URL_BASE_API_YOUTUBE: 'https://youtube.googleapis.com/youtube/v3',
-  MAX_RESULTS: 9
+  MAX_RESULTS: 9,
+  URL_BASE_API_FAVORITE_VIDEOS: 'http://localhost:8080/api/favorite-video',
 };
