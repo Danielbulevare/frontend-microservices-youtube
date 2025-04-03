6c35a9ca0dfc4c5b562e0ec7a815b700762496e2
Dan <daniel_vazrobk@hotmail.com>
2025-04-03 13:48:44 -0600

    Se modifico main.ts para que inicialice Keycloak

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { appConfig, initKeycloak } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+initKeycloak().then(() => {
+  bootstrapApplication(AppComponent, appConfig).catch((err) =>
+    console.error(err)
+  );
+});
