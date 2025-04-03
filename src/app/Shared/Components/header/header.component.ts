10b56fd4942a99bd0b4866f89671c97584cc58e3
Dan <daniel_vazrobk@hotmail.com>
2025-04-03 13:43:51 -0600

    Se modifico el html y la logica de componente header para mostrar el nombre del usuario en el header; se logro por medio del servicio de Keycloak

--- src/app/Shared/Components/header/header.component.ts
+++ src/app/Shared/Components/header/header.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { KeycloakService } from '../../../Core/Services/Keycloak/keycloak.service';
 
 @Component({
   selector: 'app-header',
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  private keycloakService = inject(KeycloakService);
 
+  async logout() {
+    this.keycloakService.logout();
+  }
+
+  get userName(): string | undefined {
+    return this.keycloakService.profile?.username;
+  }
 }
