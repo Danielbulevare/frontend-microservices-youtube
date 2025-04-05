ea4e3ac78e0d1825606d7878f494964c5d017c9e
Dan <daniel_vazrobk@hotmail.com>
2025-04-04 21:51:31 -0600

    Se agrego la ruta hacia el componente de registro de usuarios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,10 @@ export const routes: Routes = [
     loadComponent: () => import('./Auth/Pages/Login/login/login.component'),
   },
   {
+    path: 'register-account',
+    loadComponent: () => import('./Auth/Pages/Register/register/register.component'),
+  },
+  {
     path: 'microservice-youtube',
     loadComponent: () =>
       import('./microservice-youtube/microservice-youtube.component'),
@@ -30,6 +34,8 @@ export const routes: Routes = [
   {
     path: '**',
     loadComponent: () =>
-      import('./Shared/Components/PageNotFound/page-not-found/page-not-found.component')
+      import(
+        './Shared/Components/PageNotFound/page-not-found/page-not-found.component'
+      ),
   },
 ];
