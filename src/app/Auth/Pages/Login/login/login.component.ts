deddecc5a1da95a1d7ffeae3d06663fc2df83d8b
Dan <daniel_vazrobk@hotmail.com>
2025-04-04 19:12:09 -0600

    Se maqueto y dio el diseño al login. Se programaron las validaciones basicas del login

--- src/app/Auth/Pages/Login/login/login.component.ts
+++ src/app/Auth/Pages/Login/login/login.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export default class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
 
+  get email() {
+    return this.loginForm.get('email') as FormControl;
+  }
+
+  get password() {
+    return this.loginForm.get('password') as FormControl;
+  }
 }
